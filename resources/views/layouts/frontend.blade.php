754fd5afbba09a85b4aa8c35298bdcbb3aef7c60
Shift <shift@laravelshift.com>
2023-03-05 19:32:46 +0000

    Use new `@vite` Blade directive

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/frontend.css') }}">
+    @vite('resources/css/frontend.css')
     @include('frontend.partials.meta')
 </head>
 <body class="flex flex-col w-full min-h-screen font-sans antialiased text-gray-900">
 <main class="flex-grow">
     @yield('content')
 </main>
-<script src="{{ mix('js/frontend.js') }}"></script>
+@vite('resources/js/frontend.js')
 @stack('scripts')
 </body>
 </html>
