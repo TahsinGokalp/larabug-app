e5da4d58d7866b6beb94449fea86614adeafd3ad
TahsinGokalp <TahsinGokalp@users.noreply.github.com>
2023-03-07 12:22:38 +0000

    Apply fixes from StyleCI

--- tests/Feature/CreateApiTokenTest.php
+++ tests/Feature/CreateApiTokenTest.php
@@ -13,7 +13,7 @@ class CreateApiTokenTest extends TestCase
 
     public function test_api_tokens_can_be_created(): void
     {
-        if (! Features::hasApiFeatures()) {
+        if (!Features::hasApiFeatures()) {
             $this->markTestSkipped('API support is not enabled.');
 
             return;
@@ -22,7 +22,7 @@ class CreateApiTokenTest extends TestCase
         $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
         $response = $this->post('/user/api-tokens', [
-            'name' => 'Test Token',
+            'name'        => 'Test Token',
             'permissions' => [
                 'read',
                 'update',
