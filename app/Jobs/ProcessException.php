b787c92cf3400ff5d5c909cc195681bd23456598
Tahsin Gökalp Şaan <tahsinsaan@gmail.com>
2023-03-16 16:42:30 +0300

    Resolves #31 Refactor jobs

--- app/Jobs/ProcessException.php
+++ app/Jobs/ProcessException.php
@@ -25,9 +25,7 @@ class ProcessException implements ShouldQueue, ShouldBeEncrypted
 
     public Carbon $date;
 
-    public function __construct(array $data, Project $project,
-                                Carbon $date,
-                                protected HandleExceptionService $handleExceptionService)
+    public function __construct(array $data, Project $project, Carbon $date, protected HandleExceptionService $handleExceptionService)
     {
         $this->data = $data;
         $this->project = $project;
